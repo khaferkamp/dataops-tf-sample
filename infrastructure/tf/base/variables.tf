7f4361fe14e08011c82d1c0bd3d0713c7d9dc0b8
Kevin Haferkamp <gitkraken@kevinhaferkamp.com>
2022-11-05 17:53:15 +0100

    WIP

---
+++ infrastructure/tf/base/variables.tf
@@ -0,0 +1,5 @@
+variable "domain" {
+  type        = string
+  default     = "kevinhaferkamp.cloud"
+  description = "The name of the default domain"  
+}
---
+++ infrastructure/tf/dev/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+data "digitalocean_project" "dataops-project" {
+  name = "dataops-prototype"
+}
+
+variable "do_token" {}
+variable "pvt_key" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+data "digitalocean_ssh_key" "terraform" {
+  name = "terraform"
+}
+
+resource "digitalocean_project_resources" "project" {
+  project = data.digitalocean_project.dataops-project.id
+  resources = [
+    digitalocean_kubernetes_cluster.dev.urn
+  ]
+}
+
+resource "digitalocean_kubernetes_cluster" "dev" {
+  name   = "dok-cluster-dev"
+  region = "fra1"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.4-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 1
+
+    #taint {
+    #  key    = "workloadKind"
+    #  value  = "database"
+    #  effect = "NoSchedule"
+    #}
+  }
+}
