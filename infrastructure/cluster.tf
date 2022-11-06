1fcb6daf656fec981b90f82ada0644f8821b6130
Azy Sir <azysir@Azys-ET-MacBook-Pro.local>
2022-11-06 19:50:13 +0000

    Created variables for base.tfvars and dev.tfvars

--- infrastructure/cluster.tf
+++ infrastructure/cluster.tf
@@ -2,7 +2,7 @@ resource "digitalocean_kubernetes_cluster" "dataops" {
   name   = "${var.name}-${var.env}"
   region = var.region
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = var.version
+  version = var.cluster_version
 
   node_pool {
     name       = "worker-pool"
--- infrastructure/data.tf
+++
@@ -1,3 +0,0 @@
-data "digitalocean_ssh_key" "terraform" {
-  name = var.ssh_key_name
-}
