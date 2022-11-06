d00e600a988453f35594088fbe0cae2dcce356cc
Azy Sir <azysir@Azys-ET-MacBook-Pro.local>
2022-11-06 19:38:42 +0000

    Fixed folder structure

--- infrastructure/tf/base/resource.tf
+++
@@ -1,7 +0,0 @@
-resource "digitalocean_project_resources" "dataops" {
-  project = data.digitalocean_project.dataops.id
-  resources = [
-    digitalocean_domain.default.urn,
-    digitalocean_kubernetes_cluster.base.urn
-  ]
-}
