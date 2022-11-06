ee0e4cf461e661e1f8b30e303d06134ec154fcb7
Azy Sir <azysir@Azys-ET-MacBook-Pro.local>
2022-11-06 19:28:27 +0000

    Fixed more naming conventions

--- infrastructure/tf/base/resource.tf
+++ infrastructure/tf/base/resource.tf
@@ -1,4 +1,4 @@
-resource "digitalocean_project_resources" "project" {
+resource "digitalocean_project_resources" "dataops" {
   project = data.digitalocean_project.dataops.id
   resources = [
     digitalocean_domain.default.urn,
